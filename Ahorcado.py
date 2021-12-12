0df65d8675f2ab10f1cfae91b24beab9615fd875
4rzur <68611208+4rzur@users.noreply.github.com>
2021-12-12 21:38:26 +0100

    Add files via upload

---
+++ Ahorcado.py
@@ -0,0 +1,94 @@
+import random
+import os
+
+def run():
+       
+    DB = ["JAVA","PYTHON","LUA","RUST","C","BASH","PHP"]
+    IMAGENES = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+    
+    word = random.choice(DB)
+    spaces = ["_"] * len(word)
+    attemps = 6
+    dibujo = 0
+
+    while True:
+        os.system("cls")
+        for character in spaces:
+            print(character, end=" ")
+        print(IMAGENES[dibujo])
+        letter = input("Elige una letra: ").upper()
+    
+        found = False
+        for idx, character in enumerate(word):
+            if character == letter:
+                spaces[idx] = letter
+                found = True
+            
+        if not found:
+            attemps -= 1
+            dibujo += 1
+        
+        if "_" not in spaces:
+            os.system("cls")
+            print("\n\nYOU WIN !!!\n\n\n\n")
+            break
+            input()
+        
+        if attemps == 0:
+            os.system("cls")
+            print("\n\nYOU LOOSE !!!\n\n\n\n")
+            break
+            input()
+        
+if __name__ == '__main__':
+      run()
+    
---
+++ blackjack.py
@@ -0,0 +1,57 @@
+import random
+import os
+import time
+
+def run():
+
+    baraja = ["1","2","3","4","5","6","7","8","9","10","11","12"]
+
+    cartabanca1 = int(random.choice(baraja))
+    
+    cartajugador1 = int(random.choice(baraja))
+    
+    print(' *** BLACKJACK ***\n\n\n')
+    print(cartajugador1)
+    print('Es tu carta\n\n')
+    print('\n Quieres carta?\n\n')
+         
+    eleccion =input()
+    os.system("cls")
+    
+    if eleccion == 's':
+        cartajugador2 = int(random.choice(baraja))
+        print(cartajugador2)
+        time.sleep(3)
+        print(' *** BLACKJACK ***\n\n\n')
+        eleccion2 = input('Quieres seguir? ')
+        os.system("cls")
+        
+        if eleccion2 == 's':
+            total = cartajugador1 + cartajugador2
+            print(total)
+                
+            if total > 21:
+                print(' *** BLACKJACK ***\n\n\n')
+                print('Te has pasado, la banca gana...')
+            else:
+                print(' *** BLACKJACK ***\n\n\n')
+                print('Quieres otra carta? ')
+                eleccion3 = input()
+            if eleccion3 == 'n':
+                if cartabanca1 < 21:
+                    cartabanca2 = int(random.choice(baraja))
+                    totalbanca = cartabanca1 + cartabanca2
+                    print(totalbanca)
+                    time.sleep(3)
+                    if totalbanca < 21:
+                        cartabanca3 = int(random.choice(baraja))
+                        totalbanca = totalbanca + cartabanca3
+                        print(totalbanca)
+                        time.sleep(3)
+                         
+    
+    
+    else:
+        print('GANA LA BANCA !!!')
+    
+run()
